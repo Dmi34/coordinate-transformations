fd95c7af095dc68ed361d4538c5405e640233839
Dmi34 <antashkevich.dima@yandex.ru>
2024-04-23 20:46:50 +0300

    Rewrite non-inplace and add new lib structure

--- lib/transform64.h
+++ lib/transform64.h
@@ -4,68 +4,99 @@
 #include <cmath>
 #include <algorithm>
 
-struct Point64 {
+namespace DoubleGeometry {
+
+struct Point {
     double x;
     double y;
 };
 
-struct PackedPoint64 {
-    double* x;
-    double* y;
+struct PackedPoint {
+    alignas(32)
+    double x[4];
+    alignas(32)
+    double y[4];
+
+    PackedPoint() = default;
+
+    PackedPoint(const double* in_x, const double* in_y) {
+        std::copy(in_x, in_x + 4, x);
+        std::copy(in_y, in_y + 4, y);
+    }
+
+    PackedPoint(const double* source) {
+        std::copy(source, source + 4, x);
+        std::copy(source + 4, source + 8, y);
+    }
 };
 
-struct PackedSegment64 {
-    PackedPoint64 start;
-    PackedPoint64 end;
+struct PackedSegment {
+    PackedPoint start;
+    PackedPoint finish;
 };
 
-extern "C" void TranslateP64_asm(PackedPoint64* p, Point64 delta);
-extern "C" void RotateP64_asm(PackedPoint64* p, double sin, double cos);
+struct DoubleDirection {
+    double cos;
+    double sin;
 
-inline void RotateP64_asm(PackedPoint64* p, double angle) {
-    RotateP64_asm(p, std::sin(angle), std::cos(angle));
+    DoubleDirection(double in_cos, double in_sin) : cos(in_cos), sin(in_sin) {}
+
+    DoubleDirection(double angle) : cos(std::cos(angle)), sin(std::sin(angle)) {}
+};
+
+using RadianAngle = double;
+
+// Points
+extern "C" PackedPoint Translate_asm(const PackedPoint& p, Point delta);
+
+inline PackekdPoint Translate_imm(const PackedPoint& p, Point delta) {
+    PackedPoint res;
+    const auto x_new = _mm256_add_pd(_mm256_load_pd(p->x), _mm256_set1_pd(delta.x));
+    const auto y_new = _mm256_add_pd(_mm256_load_pd(p->y), _mm256_set1_pd(delta.y));
+    _mm256_store_pd(p.x, x_new);
+    _mm256_store_pd(p.y, y_new);
 }
 
-inline void TranslateP64_imm(PackedPoint64* p, Point64 delta) {
-    auto x_new = _mm256_add_pd(_mm256_load_pd(p->x), _mm256_set1_pd(delta.x));
-    auto y_new = _mm256_add_pd(_mm256_load_pd(p->y), _mm256_set1_pd(delta.y));
-    _mm256_store_pd(p->x, x_new);
-    _mm256_store_pd(p->y, y_new);
-}
-
-inline void RotateP64_imm(PackedPoint64* p, double angle) {
-    double sin = std::sin(angle);
-    double cos = std::cos(angle);
-    auto pcos = _mm256_set1_pd(cos);
-    auto psin = _mm256_set1_pd(sin);
-
-    auto x = _mm256_load_pd(p->x);
-    auto y = _mm256_load_pd(p->y);
-    auto x_new = _mm256_fmsub_pd(x, pcos, _mm256_mul_pd(y, psin)); // xcos - ysin
-    auto y_new = _mm256_fmadd_pd(x, psin, _mm256_mul_pd(y, pcos)); // xsin + ycos
-    _mm256_store_pd(p->x, x_new);
-    _mm256_store_pd(p->y, y_new);
-}
-
-inline void TranslateP64_cpp(PackedPoint64* p, Point64 delta) {
+inline PackedPoint Translate_cpp(const PackedPoint& p, Point delta) {
+    PackedPoint res;
     for (int i = 0; i < 4; i++) {
-        p->x[i] += delta.x;
-        p->y[i] += delta.y;
+        res.x[i] = p.x[i] + delta.x;
+        res.y[i] = p.y[i] + delta.y;
+    }
+    return res;
+}
+
+extern "C" PackedPoint Rotate_asm(const PackedPoint& p, DoubleDirection dir);
+
+inline PackedPoint Rotate_asm(const PackedPoint& p, RadianAngle angle) {
+    return Rotate_asm(p, DoubleDirection(angle));
+}
+
+inline PackedPoint Rotate_imm(const PackedPoint& p, RadianAngle angle) {
+    const DoubleDirection dir(angle);
+    const auto packed_cos = _mm256_set1_pd(dir.cos);
+    const auto packed_sin = _mm256_set1_pd(dir.sin);
+
+    const auto x = _mm256_load_pd(p.x);
+    const auto y = _mm256_load_pd(p.y);
+    const auto x_new = _mm256_fmsub_pd(x, pcos, _mm256_mul_pd(y, psin)); // xcos - ysin
+    const auto y_new = _mm256_fmadd_pd(x, psin, _mm256_mul_pd(y, pcos)); // xsin + ycos
+    _mm256_store_pd(p.x, x_new);
+    _mm256_store_pd(p.y, y_new);
+}
+
+inline PackedPoint Rotate_cpp(const PackedPoint& p, RadianAngle angle) {
+    const DoubleDirection dir(angle);
+    for (int i = 0; i < 4; i++) {
+        const Point mem{p.x[i], p.y[i]};
+        p.x[i] = mem.x * dir.cos - mem.y * dir.sin;
+        p.y[i] = mem.x * dir.sin + mem.y * dir.cos;
     }
 }
 
-inline void RotateP64_cpp(PackedPoint64* p, double angle) {
-    double sin = std::sin(angle);
-    double cos = std::cos(angle);
-    for (int i = 0; i < 4; i++) {
-        auto x = p->x[i];
-        auto y = p->y[i];
-        p->x[i] = x * cos - y * sin;
-        p->y[i] = x * sin + y * cos;
-    }
-}
-
-inline void TranslateS64_imm(PackedSegment64* s, Point64 delta) {
+// Segment
+/*
+inline void TranslateS64_imm(PackedSegment* s, Point delta) {
     auto packed_dx = _mm256_set1_pd(delta.x);
     auto packed_dy = _mm256_set1_pd(delta.y);
     _mm256_store_pd(s->start.x, _mm256_add_pd(_mm256_load_pd(s->start.x), packed_dx));
@@ -74,7 +105,7 @@ inline void TranslateS64_imm(PackedSegment64* s, Point64 delta) {
     _mm256_store_pd(s->end.y, _mm256_add_pd(_mm256_load_pd(s->end.y), packed_dy));
 }
 
-inline void RotateS64_imm(PackedSegment64* s, double angle) {
+inline void RotateS64_imm(PackedSegment* s, double angle) {
     double sin = std::sin(angle);
     double cos = std::cos(angle);
     auto pcos = _mm256_set1_pd(cos);
@@ -90,7 +121,7 @@ inline void RotateS64_imm(PackedSegment64* s, double angle) {
     _mm256_store_pd(s->end.y, _mm256_fmadd_pd(x2, psin, _mm256_mul_pd(y2, pcos))); // xsin + ycos
 }
 
-inline void TranslateS64_cpp(PackedSegment64* s, Point64 delta) {
+inline void TranslateS64_cpp(PackedSegment* s, Point delta) {
     for (int i = 0; i < 4; i++) {
         s->start.x[i] += delta.x;
         s->start.y[i] += delta.y;
@@ -99,7 +130,7 @@ inline void TranslateS64_cpp(PackedSegment64* s, Point64 delta) {
     }
 }
 
-inline void RotateS64_cpp(PackedSegment64* s, double angle) {
+inline void RotateS64_cpp(PackedSegment* s, double angle) {
     double sin = std::sin(angle);
     double cos = std::cos(angle);
     for (int i = 0; i < 4; i++) {
@@ -112,4 +143,6 @@ inline void RotateS64_cpp(PackedSegment64* s, double angle) {
         s->end.x[i] = x2 * cos - y2 * sin;
         s->end.y[i] = x2 * sin + y2 * cos;
     }
+}
+*/
 }
